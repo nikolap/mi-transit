7101255c6a5bab66a8684ff288c237d373bcea22
Nik Peric <nik@nikperic.com>
2023-02-11 15:47:11 +0100

    feat: initial locations api

--- src/pages/api/v1/locations.ts
+++ src/pages/api/v1/locations.ts
@@ -1,7 +1,39 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { getLocations } from "@/interfaces/transport";
+
+type QueryParam = string | string[] | undefined;
+
+function isNonBlankString(s: unknown) {
+	return typeof s === "string" && s.trim() !== "";
+}
+
+// TODO: can refactor out invalidParams and common error handling code, or use a lib
+function invalidParams(query: QueryParam) {
+	return !isNonBlankString(query);
+}
 
 async function getLocationsHandler(req: NextApiRequest, res: NextApiResponse) {
-	res.end;
+	const { query } = req.query;
+	const validationErrors = invalidParams(query);
+
+	if (validationErrors) {
+		res.status(400);
+		// TODO: more info here please
+		res.json({ error: "true" });
+	} else {
+		try {
+			const apiResponse = await getLocations({
+				query: query as string,
+				type: "all",
+			});
+			res.status(200);
+			res.json(apiResponse.stations);
+		} catch (apiError) {
+			res.status(500);
+			res.json({ error: String(apiError) });
+		}
+	}
+	res.end();
 }
 
 export default getLocationsHandler;
